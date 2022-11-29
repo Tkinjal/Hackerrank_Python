c0af2b6696eeed0d52b42117d7944618f2eaf600
Tkinjal <118758591+Tkinjal@users.noreply.github.com>
2022-11-30 00:52:30 +0530

    Add files via upload

---
+++ print i square.py
@@ -0,0 +1,19 @@
+#https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+Sample Input 0
+
+5
+Sample Output 0
+
+0
+1
+4
+9
+16
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
